9777a688c9635d80e9768f8f30e77c9f5c17be29
luiszamudio4 <rodriguez.of.luis@gmail.com>
2019-07-31 00:25:06 -0500

    added more code

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,26 +1,84 @@
-import React from 'react';
-import logo from './logo.svg';
+// Importing all components to use in App.js
+import React, { Component } from "react";
 import './App.css';
+import Navbar from "./components/Navbar";
+import Card from "./components/Card";
+import Jumbotron from "./components/Jumbotron";
+import character from "./cards.json";
+import Wrapper from "./components/Wrapper";
+import Footer from "./components/Footer";
 
-function App() {
+class App extends Component {
+  // Setting the initial state. I am using the array of artwork images that is imported from cards.json.
+  state = {
+    character,
+    clickedCharacters: [],
+    score: 0,
+    goal: 12,
+    message: ""
+  };
+  // The shuffleCharacters method compares the id of the clicked image with one that is stored in the clickedCharacters array.
+  shuffleCharacters = id => {
+    let clickedCharacters = this.state.clickedCharacters;
+
+    // If the id of the clicked artwork ...
+    if(clickedCharacters.includes(id)) {
+      this.setState({ clickedCharacters: [], score: 0, message:  "Oops - Try Again!" });
+      return;
+    } else {
+      clickedCharacters.push(id)
+
+      if(clickedCharacters.length === 12) {
+        this.setState({score: 12, message: "You Won!", clickedCharacters: []});
+        console.log('You Win');
+        return;
+      }
+
+      this.setState({ character, clickedCharacters, score: clickedCharacters.length, message: "" });
+
+      // Fisher-Yates Shuffling Algorithm for shuffling items in my character array
+      const shuffle = character => {
+        var i,
+            j,
+            temp;
+        for (i = character.length - 1; i > 0; i--) {
+            j = Math.floor(Math.random() * (i + 1));
+            temp = character[i];
+            character[i] = character[j];
+            character[j] = temp;
+        }
+         
+      };
+      shuffle(character);
+    }
+  }
+
+// Renders the imported Navbar with the current score and message, the Jumbotron,
+// the Wrapper, and the Footer. When rendering the Wrapper, it maps over this.state.character and renders a Card component for each character object.
+render() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    
+    <div>
+      <Navbar 
+      score={this.state.score}
+      message={this.state.message} 
+      />
+      <Jumbotron />
+      <Wrapper>
+        {this.state.character.map(item => (
+              <Card
+                shuffleCharacters={this.shuffleCharacters}
+                id={item.id}
+                key={item.name}
+                image={item.image}
+              />
+        ))}
+      </Wrapper>
+      <Footer />
     </div>
+        
   );
+}
 }
 
 export default App;
